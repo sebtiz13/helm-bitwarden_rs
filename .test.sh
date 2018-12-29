3dd932d6f37f85fedfccba5d22f8b680fdd7ecd3
Emil Madsen <sovende@gmail.com>
2018-12-29 18:39:15 +0000

    Fixed horrible bash scripting

--- .test.sh
+++ .test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 microk8s.kubectl port-forward $POD_NAME 31111:80 &
-until [ curl -sI localhost:31111 | grep "200 OK" ]; do
-   echo "Waiting for port-forward...";
+printf "Waiting for port-forward.";
+until $(curl --output /dev/null --silent --head --fail localhost:31111); do
+   printf '.'
+   sleep 1
 done
 
 curl -s localhost:31111 | grep "Bitwarden Web Vault"
